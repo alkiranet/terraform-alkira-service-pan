5c3de8b9fba836a701e7bad02e23d252003d3446
wcollins <william.levi.collins@gmail.com>
2022-11-26 15:40:14 -0500

    Simplify outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,4 @@
-output "id" {
-  description = "Service id"
+output "service_id" {
+  description = "ID of the service"
   value       = alkira_service_pan.service.id
-}
-
-output "cxp" {
-  description = "Service cxp"
-  value       = alkira_service_pan.service.cxp
-}
-
-output "size" {
-  description = "Service size"
-  value       = alkira_service_pan.service.size
-}
-
-output "fw_type" {
-  description = "Firewall type"
-  value       = alkira_service_pan.service.type
-}
-
-output "fw_version" {
-  description = "Firewall version"
-  value       = alkira_service_pan.service.version
 }
